4881111f6041be53309aa3c83c017e0bfff1c255
Eliza Cohen <elizacohen5@gmail.com>
2024-06-05 15:40:36 -0400

    complete python-p3-anagram-detector-lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self._word = word
+    
+    def match(self, word_list):
+        sorted_word = ''.join(sorted(self._word))
+        anagram_list = []
+        for word in word_list:
+            if sorted_word == ''.join(sorted(word)):
+                anagram_list.append(word)
+        return anagram_list
+    
+
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))
